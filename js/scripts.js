5829ff27ea27472976acf364d38a457bb708d102
堀江　凌世 <168148197+HorieRyousei@users.noreply.github.com>
2024-08-30 02:31:18 +0900

    メニューバーを作った

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('自己紹介サイトへようこそ！');
 });
+function toggleMenu() {
+    var menu = document.getElementById("slide-menu");
+    menu.classList.toggle("active");
+}
